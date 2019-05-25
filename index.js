8833351aabc8fc67522302179be45fd6a3dd26c0
nireo <eemil.lehtikangas1@gmail.com>
2019-05-25 17:07:08 +0300

    added config.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require("./utils/config.js")
 
 const blogSchema = mongoose.Schema({
     title: String,
@@ -15,6 +16,7 @@ const blogSchema = mongoose.Schema({
 const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb://localhost/bloglist'
+
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
@@ -38,7 +40,6 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
