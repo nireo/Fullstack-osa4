d3f672d5cd17224b9655bbede9f7fed305548248
nireo <eemil.lehtikangas1@gmail.com>
2019-05-30 00:56:39 +0300

    added totalLikes test & totalLikes.test.js for jest

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -1,7 +1,16 @@
+const Blog = require('../models/blog')
+
+// just a to test jest
 const dummy = (blogs) => {
     return 1;
 }
 
+// count up all the likes
+const totalLikes = (blogs) => {
+    
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
