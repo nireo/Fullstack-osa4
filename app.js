892e1456d83cc981ea0f07be0ec31052aad120a8
nireo <eemil.lehtikangas1@gmail.com>
2019-05-31 11:57:08 +0300

    added logger.js logging to replace console.logs in certain files

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const config = require("./utils/config")
 const express = require("express")
 const bodyParser = require("body-parser")
-const app = express()
-const blogsRouter = require("./controllers/blogs")
-const middleware = require("./utils/middleware")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const blogsRouter = require("./controllers/blogs")
+const middleware = require("./utils/middleware")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
+const app = express()
 
-console.log("connecting to", config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true})
     .then(() =>{
-        console.log("connected to MongoDB")
+        logger.info("connected to MongoDB")
     })
     .catch(error => {
-        console.log("error connecting to MongoDB:", error.message)
+        logger.error("error connecting to MongoDB:", error.message)
     })
 
 app.use(cors())
